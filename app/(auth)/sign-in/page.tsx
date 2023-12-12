4a2115ffab3903a00d98445ae36a7d6c447dde81
Pāvels Garklāvs <pavelsgarklavs@gmail.com>
2023-12-12 19:38:25 +0200

    layout fix

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Auth() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
       AUTH
-    </main>
+    </div>
   )
 }
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -2,8 +2,8 @@ import {SignUp} from "@/components/SignUp";
 
 export default function Auth() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignUp />
-    </main>
+    </div>
   )
 }
